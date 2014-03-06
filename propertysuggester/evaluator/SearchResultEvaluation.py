1f2eda6859c797a4e6dc9b4f74bb5a8eb527aea1
Virginia Weidhaas <flamingostern-internet@yahoo.de>
2014-03-06 15:43:19 +0100

    change internal structure, create Evaluator class

--- propertysuggester/evaluator/SearchResultEvaluation.py
+++ propertysuggester/evaluator/SearchResultEvaluation.py
@@ -20,86 +20,99 @@ import csv
 from propertysuggester.parser import CsvReader
 from propertysuggester.utils.WikidataApi import WikidataApi
 
-
-def generate_random_integers(ids):
-    counter = 0
-    while counter < 2 * sampleSize:
-        ids.append(random.randint(0, 87))
-        #ids.append(random.randint(0, 6656575))
-        counter += 1
-
-
-def handle_found_suggestion(rank):
-    global itemsOnFirst50PositionSum, itemsOnFirst50Count, appearsWithinFirst50, ranking_amounts
-    print "Found suggestion at rank " + str(rank)
-    ranking_amounts[rank] += 1
-    itemsOnFirst50PositionSum += rank
-    itemsOnFirst50Count += 1
-    appearsWithinFirst50 = True
-
-
-def write_output_file(ranking_amounts):
-    with open("_output.tsv", 'wb') as csvfile:
-        rankingwriter = csv.writer(csvfile, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        rankingwriter.writerow(["rank", "amount"])
-        for key, value in ranking_amounts.iteritems():
-            rankingwriter.writerow([str(key), str(value)])
-        rankingwriter.writerow([">50", str(itemsBeyond50Count)])
+class Evaluator():
+    def __init__(self):
+        self.inputfile = ""
+        self.outputfile = "_output.csv"
+        self.samplesize = 10
+        self.itemsBeyond50Count = 0
+        self.itemsOnFirst50PositionSum = 0
+        self.itemsOnFirst50Count = 0
+        self.appearsWithinFirst50 = False
+        self.ranking_amounts = defaultdict(lambda: 0)
+        self.ids = []
+        self.foundEntities = 0
+        
+    def generate_random_integers(self):
+        random.seed(1)
+        counter = 0
+        while counter < 2 * self.samplesize:
+            self.ids.append(random.randint(0, 87))
+            #ids.append(random.randint(0, 6656575))
+            counter += 1
 
 
-sampleSize = 2
+    def handle_found_suggestion(self, rank):
+        
+        print "Found suggestion at rank " + str(rank)
+        self.ranking_amounts[rank] += 1
+        self.itemsOnFirst50PositionSum += rank
+        self.itemsOnFirst50Count += 1
+        self.appearsWithinFirst50 = True
 
-random.seed(1)  # always choose the same random objects
-ids = []
-generate_random_integers(ids)
-print ids
-# adding 10 random integers to list
 
-itemsOnFirst50PositionSum = 0
-itemsOnFirst50Count = 0
-itemsBeyond50Count = 0
-global ranking_amounts
-ranking_amounts = defaultdict(lambda: 0)
-current_item_number = 0
-totalItems = 0
-global foundEntities
-foundEntities = 0
+    def write_output_file(self, ranking_amounts):
+        with open("_output.tsv", 'wb') as csvfile:
+            rankingwriter = csv.writer(csvfile, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            rankingwriter.writerow(["rank", "amount"])
+            for key, value in self.ranking_amounts.iteritems():
+                rankingwriter.writerow([str(key), str(value)])
+            rankingwriter.writerow([">50", str(self.itemsBeyond50Count)])
 
-with open("Wikidata-20131129161111.csv", "r") as f:
-#with open("Wikidata-20131129161111.xml.gz.csv", "r") as f:
-    for entity in CsvReader.read_csv(f):
-        if not foundEntities >= sampleSize:
-            if current_item_number in ids and len(entity.claims) > 1:
-                propertyIds = [claim.property_id for claim in entity.claims]  # get ids from claims
-                removed_list = propertyIds[:-1]
-                print "\nItem " + entity.title + ":"
-                api = WikidataApi("http://127.0.0.1/devrepo")
-                #api = WikidataApi("http://suggester.wmflabs.org/wiki")
-                suggestions = api.wbs_getsuggestions(properties=removed_list, limit=50, cont=0)
-                rank = 0
-                appearsWithinFirst50 = False
 
-                for suggestion in suggestions["search"]:
-                    rank += 1
-                    if int(propertyIds[-1]) == int(suggestion["id"][1:]):
-                        handle_found_suggestion(rank)
-                        foundEntities += 1
-                        break
-                if not appearsWithinFirst50:
-                    itemsBeyond50Count += 1
-                    foundEntities += 1
-                    print "Rank could not be determined within first 50"
-            current_item_number += 1
+    def determine_ranks(self, current_item_number, foundEntities):
+        with open("Wikidata-20131129161111.csv", "r") as f:
+        #with open("Wikidata-20131129161111.xml.gz.csv", "r") as f:
+            for entity in CsvReader.read_csv(f):
+                if not foundEntities >= self.samplesize:
+                    if current_item_number in self.ids and len(entity.claims) > 1:
+                        propertyIds = [claim.property_id for claim in entity.claims]  # get ids from claims
+                        removed_list = propertyIds[:-1]
+                        print "\nItem " + entity.title + ":"
+                        api = WikidataApi("http://127.0.0.1/devrepo")
+                        #api = WikidataApi("http://suggester.wmflabs.org/wiki")
+                        suggestions = api.wbs_getsuggestions(properties=removed_list, limit=50, cont=0)
+                        rank = 0
+                        self.appearsWithinFirst50 = False
 
-write_output_file(ranking_amounts)
-f.close()
-print "Size of Entities in Output: " + str(foundEntities)
-print " SampleSize the user requested: " + str(sampleSize)
-print "Total amount of items in dump: " + str(current_item_number)
-print "We extract the last added property and check at which position it would have been suggested (based on remaining properties)."
-if itemsOnFirst50Count > 0 and itemsOnFirst50PositionSum > 0:
-    print "Average rank of extracted properties positioned within the first 50 suggestions: " + str(
-        float(itemsOnFirst50PositionSum) / itemsOnFirst50Count)
-print "Count of extracted properties not positioned within the first 50 suggestions: " + str(itemsBeyond50Count)
+                        for suggestion in suggestions["search"]:
+                            rank += 1
+                            if int(propertyIds[-1]) == int(suggestion["id"][1:]):
+                                self.handle_found_suggestion(rank)
+                                foundEntities += 1
+                                break
+                        if not self.appearsWithinFirst50:
+                            self.itemsBeyond50Count += 1
+                            foundEntities += 1
+                            print "Rank could not be determined within first 50"
+                current_item_number += 1
+        f.close()
+        return current_item_number, f, foundEntities
 
-f.close()
+    def evaluate(self):
+        self.generate_random_integers()
+        print self.ids
+        current_item_number = 0
+        foundEntities = 0
+
+        current_item_number, f, foundEntities = self.determine_ranks(current_item_number, foundEntities)
+
+
+
+        self.write_output_file(self.ranking_amounts)
+
+        print "Size of Entities in Output: " + str(foundEntities)
+        print " samplesize the user requested: " + str(self.samplesize)
+        print "Total amount of items in dump: " + str(current_item_number)
+        print "We extract the last added property and check at which position it would have been suggested (based on remaining properties)."
+        if self.itemsOnFirst50Count > 0 and self.itemsOnFirst50PositionSum > 0:
+            print "Average rank of extracted properties positioned within the first 50 suggestions: " + str(
+                float(self.itemsOnFirst50PositionSum) / self.itemsOnFirst50Count)
+        print "Count of extracted properties not positioned within the first 50 suggestions: " + str(self.itemsBeyond50Count)
+
+        f.close()
+
+
+
+x = Evaluator()
+x.evaluate()
