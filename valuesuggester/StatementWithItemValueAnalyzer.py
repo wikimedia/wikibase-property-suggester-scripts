71babd5a650e47afc3bd687b0549f8e02b5b4ccf
Felix Niemeyer <felix.niemeyer@student.hpi.uni-potsdam.de>
2014-04-09 14:20:09 +0200

    resolving bugs

--- valuesuggester/StatementWithItemValueAnalyzer.py
+++ valuesuggester/StatementWithItemValueAnalyzer.py
@@ -10,12 +10,13 @@ class StatementWithItemValueAnalyzer:
         print "\n\n#step1: count statement occurences\n"
         statementGroups = defaultdict()
         numOfStatements = self.countStatementOccurences(statementsFile, statementGroups)
+    	
+        coverage = self.calculateMinCoverage(len(statementGroups), numOfStatements)
+        print "coverage = {0}\n".format(coverage)
 
         print "\n\n#step2: apply selection of relevant statements\n"
         relevantStatementGroups = defaultdict()
-        maxEntries = numOfStatements**0.5
-        self.applyCutoff(statementGroups, relevantStatementGroups, maxEntries)
-
+        self.applyThreshold(statementGroups, relevantStatementGroups, coverage)
         print "{0} of {1} statements do show a sufficient coverage\n".format(len(relevantStatementGroups),len(statementGroups))
         statementGroups.clear() #free memory
 
@@ -24,12 +25,14 @@ class StatementWithItemValueAnalyzer:
         self.initializeStatementPairGroups(relevantStatementGroups.keys(), statementPairGroups)
 
         print "\n\n#step4: count statement pair appearences\n"
-        self.countStatementPairOccurences(statementsFile, statementPairGroups)
+        numOfStatementPairs = self.countStatementPairOccurences(statementsFile, statementPairGroups)
 
         print "\n\n#step5: apply selection of relevant statements\n"
+        avgStatementsPerGroup = numOfStatementPairs/len(statementPairGroups)
+        coverage = 20*avgStatementsPerGroup
         relevantStatementPairGroups = defaultdict()
-        self.applyCutoff(statementPairGroups, relevantStatementPairGroups, coverage)
-        print "{0} of {1} statements do show a sufficient coverage\n".format(len(relevantStatementPairGroups),len(statementPairGroups))
+        self.applyThreshold(statementPairGroups, relevantStatementPairGroups, coverage)
+        print "{0} of {1} statement pairs do show a sufficient coverage\n".format(len(relevantStatementPairGroups),len(statementPairGroups))
         statementPairGroups.clear()
 
         print "\n\n#step6: write Things to disk\n"
@@ -47,13 +50,17 @@ class StatementWithItemValueAnalyzer:
                 
         return i
 
-    def applyCutoff(self, inList, outList, maxEntries):
-        i = 0
-        for k, v in sorted(inList.items(), key=lambda x:x[1]):
-            outList[k] = v 
-            i += 1
-            if i >= maxEntries:
-                break
+    def calculateMinCoverage(self, numOfGorups, numOfStatements):
+        """ select threshold so that n will be so that we'll select n statement groups as relevant where n x n = numOfStatements."""
+        # todo find adequate distribution
+        avg = numOfStatements*1.0/numOfGorups
+        # proportion = numOfStatements**-0.5
+        return avg * 65
+
+    def applyThreshold(self, inList, outList, threshold):
+        for k, v in inList.items():
+            if v >= threshold:
+                outList[k] = v 
 
     def initializeStatementPairGroups(self, keyValuePairs, statementPairGroup):
         for pair1 in keyValuePairs:
@@ -74,15 +81,16 @@ class StatementWithItemValueAnalyzer:
                     keyPair = (pair1, pair2)
                     if keyPair in statementPairGroups:
                         statementPairGroups[keyPair] += 1
-            i += 1
-            if 0 == (i % 1000000): print "processed {0} items\n".format(i)
+                        i += 1
+                        if 0 == (i % 1000000): print "processed {0} relevant property pair occurences\n".format(i)
+        return i
 
     def writeInfo(self, relevantStatementGroups, outFilePrefix):
         fs = open(outFilePrefix+"statement_occurences.data", "w")
         for sg in relevantStatementGroups:
-            fs.write(sg+"\n")
+            fs.write(str(sg)+"\n")
 
     def writeRules(self, relevantStatementPairGroups, outFilePrefix):
         fs = open(outFilePrefix+"statement_pair_occurences.data", "w")
         for spg in relevantStatementPairGroups:
-            fs.write(spg+"\n")
+            fs.write(str(spg)+"\n")
