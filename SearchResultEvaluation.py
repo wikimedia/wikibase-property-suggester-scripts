9306b09bda787c7ae265ef1617aa1bc1f6d98f1d
Dacry <moritz.finke@student.hpi.uni-potsdam.de>
2014-06-26 19:30:21 +0200

    ...

--- SearchResultEvaluation.py
+++ SearchResultEvaluation.py
@@ -9,7 +9,7 @@ class SearchResultEvaluation(ResultEvaluation):
     def __init__(self):
         self.filename = "E:\Moritz\Desktop\Bachelorprojekt\wikidatawiki-20140526-pages-articles.xml\dump.csv"
         self.samplesize = 1000
-        self.outputfile = "ClassifyingPropertiesEvaluation20130526_dump" + str(self.samplesize) + "6_threshold_0_.csv"
+        self.outputfile = "ClassifyingPropertiesEvaluation20130526_dump" + str(self.samplesize) + "2To6_threshold_0_.csv"
 
         ResultEvaluation.__init__(self, self.filename,
 								  self.samplesize , self.outputfile)
